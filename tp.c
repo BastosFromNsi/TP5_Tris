7aa254ce1bf7176320f1d9657bbc167addab2d51
Bastien Le Corguille <bastien.bruges@icloud.com>
2025-04-07 11:27:35 +0200

    Ex 1 Question 2 - Recherche dichotomique (pas completement optimisé)

--- tp.c
+++ tp.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void tri_a_bulles(Medicament *tab, int n)
+int tri_a_bulles(Medicament *tab, int n)
 {
     if (tab == NULL || n <= 0)
     {
-        return -1;
+        return 1;
     }
 
     int i, j;
@@ -23,6 +23,48 @@ void tri_a_bulles(Medicament *tab, int n)
             }
         }
     }
+
+    return 0;
+}
+
+void recherche_dichotomique(Medicament *tab, int n)
+{
+
+    int debut = 0;
+    int fin = n - 1;
+    int milieu;
+    int trouve = 0;
+    char nom_medoc[20];
+    fgets(nom_medoc, sizeof(nom_medoc), stdin);
+    getchar();
+    while (trouve != 1 && debut <= fin)
+    {
+        milieu = ((debut + fin) / 2);
+        if ((tab[milieu]).nom == nom_medoc)
+        {
+            trouve = 1;
+        }
+        else
+        {
+            if (nom_medoc > (tab[milieu].nom))
+            {
+                debut = milieu + 1;
+            }
+            else
+            {
+                fin = milieu - 1;
+            }
+        }
+    }
+
+    if (trouve == 1)
+    {
+        printf(" La valeur %s est au rang %d /n", nom_medoc, milieu);
+    }
+    else
+    {
+        printf("La valeur %s n'est pas dans le tableau", nom_medoc);
+    }
 }
 
 int main()
@@ -31,17 +73,17 @@ int main()
     int n = 4;
 
     Medicament tab[] = {
-        {"Medicament1", 101, 20230101, 20220302, 100, 1, 50},
-        {"Medicament2", 102, 20230201, 20240701, 200, 2, 30},
-        {"Medicament3", 103, 20230401, 20240301, 150, 1, 20},
-        {"Medicament4", 104, 20230301, 20240401, 250, 3, 10},
+        {"Paracetamol", 101, 20230101, 20220302, 100, 1, 50},
+        {"Ibuprofène", 102, 20230201, 20240701, 200, 2, 30},
+        {"Aspirine", 103, 20230401, 20240301, 150, 1, 20},
+        {"Fluoxétine", 104, 20230301, 20240401, 250, 3, 10},
     };
 
     printf("Tableau avant le tri :\n");
     for (int i = 0; i < n; i++)
     {
-        printf(" Nom : %s, Code : %d, Date de prevention : %d, Date de peremption : %d, Prix : %d, Unite : %d, Stock restant : %d\n",
-               tab[i].nom, tab[i].code, tab[i].date_prevention, tab[i].date_peremption, tab[i].prix, tab[i].unite, tab[i].stock_restant);
+        printf(" Nom : %s, Code : %d, Date de fabrication : %d, Date de peremption : %d, Prix : %d, Unite : %d, Stock restant : %d\n",
+               tab[i].nom, tab[i].code, tab[i].date_fabrication, tab[i].date_peremption, tab[i].prix, tab[i].unite, tab[i].stock_restant);
     }
 
     tri_a_bulles(tab, n);
@@ -50,9 +92,11 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        printf(" Nom : %s, Code : %d, Date de prevention : %d, Date de peremption : %d, Prix : %d, Unite : %d, Stock restant : %d\n",
-               tab[i].nom, tab[i].code, tab[i].date_prevention, tab[i].date_peremption, tab[i].prix, tab[i].unite, tab[i].stock_restant);
+        printf(" Nom : %s, Code : %d,  : %d, Date de fabrication : %d, Prix : %d, Unite : %d, Stock restant : %d\n",
+               tab[i].nom, tab[i].code, tab[i].date_fabrication, tab[i].date_peremption, tab[i].prix, tab[i].unite, tab[i].stock_restant);
     }
+
+    recherche_dichotomique(tab, n);
 
     return 0;
 }
--- tp.h
+++ tp.h
@@ -8,14 +8,15 @@ typedef struct
 {
     char nom[20];
     int code;
-    int date_prevention;
+    int date_fabrication;
     int date_peremption;
     int prix;
     int unite;
     int stock_restant;
 }Medicament;
 
-void tri_a_bulles(Medicament *tab, int n);
+int tri_a_bulles(Medicament *tab, int n);
+void recherche_dichotomique(Medicament *tab, int n);
 
 
 #endif
